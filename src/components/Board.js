78d8b1478a7a963c9d364c54871a38ef186ea515
WarrenMfg <warrenmfg@icloud.com>
2021-01-23 07:19:09 -0500

    adds basic card matching state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from './Card';
 
@@ -8,42 +8,102 @@ import './styles/Board.css';
  * Board component - stateful container
  */
 function Board() {
-  const [emojis] = useState([
-    '😀',
-    '😂',
-    '😘',
-    '😜',
-    '🧐',
-    '🤓',
-    '😎',
-    '🥳',
-    '🥴',
-    '🤑',
-    '😳',
-    '🙄'
-  ]);
+  // track active cards
+  const [activeCards, setActiveCards] = useState([]);
+  // track number of matched cards
+  const [matches, setMatches] = useState(0);
+  // track attempts
+  const [attempts, setAttempts] = useState(0);
 
-  // map emojis twice to render 2 of each
+  console.log(activeCards, matches, attempts);
+
+  // track activeCards
+  useEffect(() => {
+    // when empty
+    if (!activeCards.length) return;
+    // if only one card
+    if (activeCards.length < 2) {
+      // increment attempts
+      setAttempts(attempts + 1);
+      return;
+    }
+
+    // if cards match
+    if (activeCards[0].slice(0, -2) === activeCards[1].slice(0, -2)) {
+      // increment matches
+      setMatches(matches + 1);
+      // increment attempts
+      setAttempts(attempts + 1);
+      // setTimeout for card turnover animation
+      // TODO
+
+      // otherwise, if cards do not match
+    } else {
+      // only increment attempts
+      setAttempts(attempts + 1);
+      // setTimeout for card turnover animation
+      // TODO
+    }
+  }, [activeCards]);
+
+  const handleCardClick = e => {
+    // get closest card and ensure a card was actually clicked
+    const target = e.target.closest('.card');
+    // if not, return
+    if (!target) return;
+    // if already active, return
+    if (activeCards.includes(target.dataset.emoji)) return;
+    // finally, if can make active
+    if (activeCards.length < 2) {
+      // make active
+      setActiveCards([...activeCards, target.dataset.emoji]);
+    }
+  };
+
+  // render emojis twice so they have a match
   return (
-    <div className='board h-100 d-flex flex-wrap justify-content-center align-items-center'>
-      {emojis.map((emoji, i) => (
-        <div
-          key={`${i}-${emoji}`}
-          className='card-container d-flex justify-content-center align-items-center p-1 col-3 col-md-2'
-        >
-          <Card emoji={emoji} />
-        </div>
-      ))}
-      {emojis.map((emoji, i) => (
-        <div
-          key={`${i}-${emoji}`}
-          className='card-container d-flex justify-content-center align-items-center p-1 col-3 col-md-2'
-        >
-          <Card emoji={emoji} />
-        </div>
-      ))}
+    <div
+      className='board h-100 d-flex flex-wrap justify-content-center align-items-center'
+      onClick={handleCardClick}
+    >
+      {emojis.reduce((acc, emoji) => {
+        const one = (
+          <Card
+            emoji={emoji}
+            key={`${emoji}-1`}
+            dataEmoji={`${emoji}-1`}
+            activeCards={activeCards}
+          />
+        );
+        const two = (
+          <Card
+            emoji={emoji}
+            key={`${emoji}-2`}
+            dataEmoji={`${emoji}-2`}
+            activeCards={activeCards}
+          />
+        );
+        acc.push(one, two);
+        return acc;
+      }, [])}
     </div>
   );
 }
+
+// a list of emojis to use
+const emojis = [
+  '😀',
+  '😂',
+  '😘',
+  '😜',
+  '🧐',
+  '🤓',
+  '😎',
+  '🥳',
+  '🥴',
+  '🤑',
+  '😳',
+  '🙄'
+];
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,28 @@ import './styles/Card.css';
 /**
  * Card component - basic game piece
  */
-function Card({ emoji }) {
+function Card({ emoji, dataEmoji, activeCards }) {
+  // determine if this component is active
+  const isActive = activeCards.includes(dataEmoji);
+
   return (
-    <div className='card w-100 h-100 d-flex justify-content-center align-items-center'>
-      {emoji}
+    <div
+      className={`${isActive} card-container d-flex justify-content-center align-items-center p-1 col-3 col-md-2`}
+    >
+      <div
+        data-emoji={dataEmoji}
+        className='card w-100 h-100 d-flex justify-content-center align-items-center'
+      >
+        {isActive && emoji}
+      </div>
     </div>
   );
 }
 
 Card.propTypes = {
-  emoji: PropTypes.string.isRequired
+  emoji: PropTypes.string.isRequired,
+  dataEmoji: PropTypes.string.isRequired,
+  activeCards: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
 };
 
 export default Card;
